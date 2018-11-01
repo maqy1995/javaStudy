93053092f344457d89abf344b00eb18dc0d992e6
maqy1995 <maqy1995@163.com>
2018-11-02 00:44:05 +0800

    rename collectionTest to collectionDemo

---
+++ src/collectionDemo/CollectionsTest.java
@@ -0,0 +1,9 @@
+package collectionDemo;
+
+import java.util.AbstractCollection;
+import java.util.Collection;
+
+public class CollectionsTest  {
+    Collection c;
+    AbstractCollection abstractCollection;
+}
---
+++ src/collectionDemo/Person.java
@@ -0,0 +1,55 @@
+package collectionDemo;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    @Override
+    public int hashCode() {
+        //在name和age相同时返回相同的hashCode
+        return Objects.hash(this.name,this.age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj==this){
+            return true;
+        }
+        if(obj instanceof Person){
+            Person p = (Person) obj;
+            //引用类型需要先判断是否为null，不是null再用equals方法判断是否相等，所以可以用Objects.equals()方法
+            if(Objects.equals(this,p)&& p.age==this.age){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
